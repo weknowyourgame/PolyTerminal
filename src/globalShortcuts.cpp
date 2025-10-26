6e30dd06d48e05dec0117f44aebed816d021fc78
Sarthak <aniki123.ai@gmail.com>
2025-10-26 23:22:18 +0530

    feat: change pos with keyboard

--- src/globalShortcuts.cpp
+++ src/globalShortcuts.cpp
@@ -5,7 +5,9 @@ GlobalKeyFilter::GlobalKeyFilter(QMainWindow *win) : mainWin(win) {}
 bool GlobalKeyFilter::eventFilter(QObject *watched, QEvent *event) {
     if (event->type() == QEvent::KeyPress) {
         QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+        QPoint pos = mainWin->pos();
 
+        // Inc window size
         if ((keyEvent->key() == Qt::Key_Plus || keyEvent->key() == Qt::Key_Equal) && (keyEvent->modifiers() & Qt::ControlModifier)) {
             QSize s = mainWin->size();
             mainWin->resize(s.width() + 20, s.height() + 20);
@@ -13,10 +15,39 @@ bool GlobalKeyFilter::eventFilter(QObject *watched, QEvent *event) {
             return true;
         }
 
+        // Dec window size
         if (keyEvent->key() == Qt::Key_Minus && (keyEvent->modifiers() & Qt::ControlModifier)) {
             QSize s = mainWin->size();
             mainWin->resize(s.width() - 20, s.height() - 20);
             qDebug() << "Decreased window size to" << mainWin->size();
+            return true;
+        }
+
+        // Move left
+        if (keyEvent->key() == Qt::Key_Left && (keyEvent->modifiers() & Qt::ControlModifier)) {
+            mainWin->move(pos.x() - 10, pos.y());
+            qDebug() << "Window moved left, curr pos:" << mainWin->pos();
+            return true;
+        }
+
+        // Move right
+        if (keyEvent->key() == Qt::Key_Right && (keyEvent->modifiers() & Qt::ControlModifier)) {
+            mainWin->move(pos.x() + 10, pos.y());
+            qDebug() << "Window moved right, curr pos:" << mainWin->pos();
+            return true;
+        }
+
+        // Move up
+        if (keyEvent->key() == Qt::Key_Up && (keyEvent->modifiers() & Qt::ControlModifier)) {
+            mainWin->move(pos.x(), pos.y() - 10);
+            qDebug() << "Window moved up, curr pos:" << mainWin->pos();
+            return true;
+        }
+
+        // Move down
+        if (keyEvent->key() == Qt::Key_Down && (keyEvent->modifiers() & Qt::ControlModifier)) {
+            mainWin->move(pos.x(), pos.y() + 10);
+            qDebug() << "Window moved down, curr pos:" << mainWin->pos();
             return true;
         }
     }
--- src/globalShortcuts.h
+++ src/globalShortcuts.h
@@ -8,10 +8,8 @@
 class GlobalKeyFilter : public QObject {
     Q_OBJECT
     QMainWindow *mainWin;
-
 public:
     GlobalKeyFilter(QMainWindow *win);
-
 protected:
     bool eventFilter(QObject *watched, QEvent *event) override;
 };
