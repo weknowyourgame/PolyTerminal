751e2b1f90919bf00491fdeff36119cb6150c40c
Sarthak <aniki123.ai@gmail.com>
2025-10-28 07:30:34 +0530

    fix: cmake

--- Api/main.cpp
+++ Api/main.cpp
@@ -10,13 +10,19 @@ using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
 
-// Logic and data behind the server's behavior.
-class ClobServiceImpl final : public clob::Service {
+class StatusServiceImpl final : public polymarket::clob::status::Service {
+public:
+    grpc::Status CheckStatus(grpc::ServerContext* context, const polymarket::clob::Empty* request, polymarket::clob::ClobApiStatus* response) override {
+        response->set_status("Healthy");
+
+        std::cout << "[CheckStatus] Called, returning Healthy" << std::endl;
+        return grpc::Status::OK;
+    }
 };
 
 void RunServer() {
     std::string server_address("0.0.0.0:8888");
-    ClobServiceImpl clob_service;
+    StatusServiceImpl status_service;
 
     ServerBuilder builder;
     // Listen on the given address without any authentication mechanism.
@@ -24,7 +30,7 @@ void RunServer() {
     
     // Register "service" as the instance through which we'll communicate with
     // clients. In this case it corresponds to an *synchronous* service.
-    builder.RegisterService(&clob_service);
+    builder.RegisterService(&status_service);
     
     // Finally assemble the server.
     std::unique_ptr<Server> server(builder.BuildAndStart());
